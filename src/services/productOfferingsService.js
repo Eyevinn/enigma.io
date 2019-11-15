53fe5f3059e1fb2ecdc250b65236f705dea7172f
birme <jonas.birme@eyevinn.se>
2019-11-15 13:21:04 +0100

    List product offerings

--- src/services/productOfferingsService.js
+++ src/services/productOfferingsService.js
@@ -1,7 +1,18 @@
 const fetch = require("node-fetch");
+const debug = require("debug")("product-offerings-service");
 
 const getOfferings = async ({ url, bearerToken }) => {
-  
+  let records = [];
+  let response = await fetch(url, {
+    headers: { Authorization: `Basic ${bearerToken}` }
+  });
+  let json = await response.json();
+  if (response.ok) {
+    await records.push.apply(records, json);
+    return records;
+  } else {
+    throw json.message;
+  }
 };
 
 module.exports = {
