0c58354902ca5b298ccb7d9711e0c75f68665ea6
Jonas Birmé <jonas.birme@eyevinn.se>
2019-11-18 16:27:21 +0100

    Tool to obtain a session token for a user. (#1)
    * Tool to obtain a session token for a user.
    
    * Added auth api to readme
    
    * Device name should be SDK and not CLI
    
    * Lint fixes

---
+++ src/api/exposure.js
@@ -0,0 +1,24 @@
+//const debug = require("debug")("mgmt");
+
+const EXPOSURE_API_ENDPOINT = "https://exposureapi.emp.ebsd.ericsson.net/v2";
+
+const authService = require("../services/authService");
+
+class ExposureAPI {
+  constructor(customerUnit, businessUnit) {
+    this.customerUnit = customerUnit;
+    this.businessUnit = businessUnit;
+  }
+
+  async authenticate(username, password) {
+    if (!this.customerUnit || !this.businessUnit) return;
+    const url = `${EXPOSURE_API_ENDPOINT}/customer/${this.customerUnit}/businessunit/${this.businessUnit}/auth/login`;
+    return await authService.authenticate({
+      url,
+      username,
+      password
+    });
+  }
+}
+
+module.exports = ExposureAPI;
