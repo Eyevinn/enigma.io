4cc7f40cbc39072969c699cdc0e4be5a3fec038a
Jonas Birmé <jonas.birme@eyevinn.se>
2019-11-20 08:03:52 +0100

    A script to obtain a playback URL (#2)
    * A script to obtain a playback URL
    
    * added quotes for sessiontoken in the help example

---
+++ src/services/entitlementService.js
@@ -0,0 +1,21 @@
+const fetch = require("node-fetch");
+const debug = require("debug")("entitlement-service");
+
+const play = async ({ url, sessionToken }) => {
+  const response = await fetch(url, {
+    headers: {
+      Authorization: "Bearer " + sessionToken
+    }
+  });
+  const playResponse = await response.json();
+  if (response.ok) {
+    return playResponse;
+  } else {
+    debug(playResponse);
+    throw playResponse.message;
+  }
+};
+
+module.exports = {
+  play
+};
