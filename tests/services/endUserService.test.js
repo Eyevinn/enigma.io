9c2682dfa14271e174e3cfd863c7bb731a785d4a
Erik Hoffman <erik.johansson87@gmail.com>
2019-11-21 08:47:43 +0100

    tests for endUserService

---
+++ tests/services/endUserService.test.js
@@ -0,0 +1,63 @@
+const endUserService = require("../../src/services/endUserService");
+const base64 = require("base-64");
+
+const MGMT_API_ENDPOINT = "https://managementapi.emp.ebsd.ericsson.net/v2";
+const customerUnit = "Eyevinn";
+const businessUnit = "STSWE";
+const apiKeyId = process.env.API_KEY_ID;
+const apiKeySecret = process.env.API_KEY_SECRET;
+const bearerToken =
+  apiKeyId && apiKeySecret
+    ? base64.encode(`${apiKeyId}:${apiKeySecret}`)
+    : null;
+
+describe("endUserService", () => {
+  describe("getUsers", () => {
+    it("should respond with a list of 5 users with limit 5", async () => {
+      if (!bearerToken || !customerUnit || !businessUnit) return;
+      const url = `${MGMT_API_ENDPOINT}/customer/${customerUnit}/businessunit/${businessUnit}/enduseraccount/user`;
+      const limit = 5;
+      const usersResponse = await endUserService.getUsers({
+        url,
+        bearerToken,
+        limit
+      });
+      expect(usersResponse).toBeTruthy();
+      expect(usersResponse.length).toBe(5);
+    });
+
+    it("should give UNKNOWN_BUSINESS_UNIT if incorrect BU", async () => {
+      if (!bearerToken || !customerUnit || !businessUnit) return;
+      const url = `${MGMT_API_ENDPOINT}/customer/${customerUnit}/businessunit/${customerUnit}/enduseraccount/user`;
+      const limit = 5;
+      let errorThrown = false;
+      try {
+        await endUserService.getUsers({
+          url,
+          bearerToken,
+          limit
+        });
+      } catch (error) {
+        errorThrown = true;
+      }
+      expect(errorThrown).toBeTruthy();
+    });
+
+    it("should throw error for incorrect credentials", async () => {
+      if (!bearerToken || !customerUnit || !businessUnit) return;
+      const url = `${MGMT_API_ENDPOINT}/customer/${customerUnit}/businessunit/${businessUnit}/enduseraccount/user`;
+      const limit = 5;
+      let errorThrown = false;
+      try {
+        await endUserService.getUsers({
+          url,
+          bearerToken: "12345",
+          limit
+        });
+      } catch (error) {
+        errorThrown = true;
+      }
+      expect(errorThrown).toBeTruthy();
+    });
+  });
+});
