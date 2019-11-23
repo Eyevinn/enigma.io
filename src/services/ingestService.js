4095abb7a787da5ef3e3ad9009c67f6baf58f87f
Erik Hoffman <erik.johansson87@gmail.com>
2019-11-23 14:02:39 +0100

    moved out xml templating to its own utils file

--- src/services/ingestService.js
+++ src/services/ingestService.js
@@ -1,24 +1,11 @@
 const fetch = require("node-fetch");
 const uuid = require("uuid/v1");
 const debug = require("debug")("ingest-service");
+const templates = require("../utils/templates");
 
 const createAsset = async ({ url, title, bearerToken }) => {
   const id = uuid();
-  const ingestRequestXML = 
-    '<?xml version="1.0" encoding="UTF-8"?>' +
-    '<publish-metadata xmlns="http://video-metadata.emp.ebsd.ericsson.net/publish-metadata/v1"' +
-    '  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' +
-    '  xsi:schemaLocation="http://video-metadata.emp.ebsd.ericsson.net/publish-metadata/v1/../xsd/publish-metadata.xsd">' +
-    '<data>' +
-    '  <asset>' +
-    `    <id>${id}</id>` +
-    '    <titleList>' +
-    `      <title language="en">${title}</title>` +
-    '    </titleList>' +
-    '    <assetType>movie</assetType>' +
-    '  </asset>' +
-    '</data>' +
-    '</publish-metadata>';
+  const ingestRequestXML = templates.assetIngestXML({ id, title });
 
   const response = await fetch(url, {
     headers: {
@@ -38,20 +25,7 @@ const createAsset = async ({ url, title, bearerToken }) => {
 
 const ingestVideo = async ({ url, assetId, videoUrl, bearerToken }) => {
   const id = uuid();
-  const ingestRequestXML =
-    '<?xml version="1.0" encoding="UTF-8"?>' +
-    '<publish-metadata xmlns="http://video-metadata.emp.ebsd.ericsson.net/publish-metadata/v1"' +
-    '  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' +
-    '  xsi:schemaLocation="http://video-metadata.emp.ebsd.ericsson.net/publish-metadata/v1/../xsd/publish-metadata.xsd">' +
-    '<data>' +
-    '  <material>' +
-    `    <id>${id}</id>` +
-    `    <materialRef>${videoUrl}</materialRef>` +
-    `    <assetIdRef>${assetId}</assetIdRef>` +
-    '    <DRMEnabled>false</DRMEnabled>' +
-    '  </material>' +
-    '</data>' +
-    '</publish-metadata>';
+  const ingestRequestXML = templates.videoIngestXML({ id, videoUrl, assetId });
 
   const response = await fetch(url, {
     headers: {
---
+++ src/utils/templates.js
@@ -0,0 +1,39 @@
+const assetIngestXML = ({ id, title }) => {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+    <publish-metadata xmlns="http://video-metadata.emp.ebsd.ericsson.net/publish-metadata/v1"
+     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+     xsi:schemaLocation="http://video-metadata.emp.ebsd.ericsson.net/publish-metadata/v1/../xsd/publish-metadata.xsd">
+    <data>
+      <asset>
+        <id> ${id}</id>
+        <titleList>
+          <title language="en" > ${title}</title >
+        </titleList>
+        <assetType>movie</assetType>
+      </asset>
+    </data>
+    </publish-metadata>
+  `;
+};
+
+const videoIngestXML = ({ id, videoUrl, assetId }) => {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+    <publish-metadata xmlns="http://video-metadata.emp.ebsd.ericsson.net/publish-metadata/v1"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://video-metadata.emp.ebsd.ericsson.net/publish-metadata/v1/../xsd/publish-metadata.xsd">
+    <data>
+      <material>
+        <id> ${id}</id>
+        <materialRef>${videoUrl}</materialRef>
+        <assetIdRef>${assetId}</assetIdRef>
+        <DRMEnabled>false</DRMEnabled>
+      </material>
+    </data>
+    </publish-metadata>
+  `;
+};
+
+module.exports = {
+  assetIngestXML,
+  videoIngestXML
+};
