23447182cc6ca6c41393de84515050d2000234ab
Erik Hoffman <erik.johansson87@gmail.com>
2020-04-21 07:43:57 +0200

    exposes both classes as the editor can track available methods etc better during dev

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // API_KEY_ID =
 // API_KEY_SECRET =
 
-function setup(api) {
-  const APIs = {
-    mgmt: "./src/api/mgmt.js",
-    exposure: "./src/api/exposure.js"
-  };
-  const sdk = require(APIs[api]);
-  return sdk;
-}
+const EnigmaManagementAPI = require("./src/api/mgmt");
+const ExposureAPI = require("./src/api/exposure");
 
-module.exports = setup;
+module.exports = {
+  ManagementAPI: EnigmaManagementAPI,
+  ExposureAPI
+};
+
+// function setup(api) {
+//   const APIs = {
+//     mgmt: "./src/api/mgmt.js",
+//     exposure: "./src/api/exposure.js"
+//   };
+//   const sdk = require(APIs[api]);
+//   return sdk;
+// }
+
+// module.exports = setup;
