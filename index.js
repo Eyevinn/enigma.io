558d52376352f00101dd0bdae5c622361e62344a
Erik Hoffman <erik.johansson87@gmail.com>
2020-04-21 09:07:51 +0200

    cleanup some commented code

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// API_KEY_ID =
-// API_KEY_SECRET =
-
 const EnigmaManagementAPI = require("./src/api/mgmt");
 const ExposureAPI = require("./src/api/exposure");
 
@@ -8,14 +5,3 @@ module.exports = {
   ManagementAPI: EnigmaManagementAPI,
   ExposureAPI
 };
-
-// function setup(api) {
-//   const APIs = {
-//     mgmt: "./src/api/mgmt.js",
-//     exposure: "./src/api/exposure.js"
-//   };
-//   const sdk = require(APIs[api]);
-//   return sdk;
-// }
-
-// module.exports = setup;
